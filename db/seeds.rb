5d78258ff4af151a5d7213631dd899c73913ddfa
Saori Mita <saorimita4@gmail.com>
2022-04-08 08:51:38 +0900

    done with seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,23 @@
 require 'open-uri'
+require 'json'
+
+puts "Cleaning up database..."
 Movie.destroy_all
-# List.destroy_all
+puts "Database cleaned"
 
-# the Le Wagon copy of the API
-url = 'http://tmdb.lewagon.com/movie/top_rated'
-response = JSON.parse(URI.open(url).read)
-
-response['results'].each do |movie_hash|
-  puts
-  p movie_hash
-  # create an instance with the hash
-  # Movie.create!(
-  #   poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path']
-  #   ...
-  # )
+url = "http://tmdb.lewagon.com/movie/top_rated"
+10.times do |i|
+  puts "Importing movies from page #{i + 1}"
+  movies = JSON.parse(URI.open("#{url}?page=#{i + 1}").read)['results']
+  movies.each do |movie|
+    puts "Creating #{movie['title']}"
+    base_poster_url = "https://image.tmdb.org/t/p/original"
+    Movie.create(
+      title: movie['title'],
+      overview: movie['overview'],
+      poster_url: "#{base_poster_url}#{movie['backdrop_path']}",
+      rating: movie['vote_average']
+    )
+  end
 end
+puts "Movies created"
