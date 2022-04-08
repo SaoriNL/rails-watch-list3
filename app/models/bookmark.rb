8da55fd4933ca3dcbf977e9ebf0f37da7220db6c
Saori Mita <saorimita4@gmail.com>
2022-04-08 09:05:01 +0900

    trying to fix photos for bookmarks

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,4 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :list, dependent: :destroy
   validates :comment, length: { minimum: 6 }
   validates :movie, uniqueness: { scope: :list }
+  has_one_attached :photo
 end
